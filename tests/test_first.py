1f1d54b21b8ec5e2b367d5bed01be4517198328d
JeanCarloMachado <jean.machado@getyourguide.com>
2021-11-25 17:42:17 +0100

    mypy not failing

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,41 +1,54 @@
-from pyspark.sql.session import SparkSession
-from typed_pyspark import DataFrame, class_annotation
+# type: ignore[return-value,type-arg]
+from __future__ import annotations
+
+from typing import Any, NewType
+
 import pyspark.sql.functions as F
+from pyspark.sql.session import SparkSession
+
+from typed_pyspark import DataFrameNew, class_annotation
 
 spark = SparkSession.builder.getOrCreate()
 
+
 @class_annotation
-class User(DataFrame):
+class User(DataFrameNew):
     name: str
     age: str
 
-Foo = class_annotation(DataFrame['abc'])
 
 users_data = [
-    {'name': 'user1', 'age': 29},
-    {'name': 'user2', 'age': 30},
-    {'name': 'user1', 'age': 15},
+    {"name": "user1", "age": 29},
+    {"name": "user2", "age": 30},
+    {"name": "user1", "age": 15},
 ]
 
 users = User.from_data(users_data)
-SumResult = DataFrame['name': type(str), 'age']
+
+name = NewType("name", str)
+age = NewType("age", int)
+SumResult = DataFrameNew["name", "age"]
+
 
 def avg_age(df: User) -> SumResult:
-    return df.groupBy('name').agg(F.sum('age').alias('sum'))
+    return df.groupBy("name").agg(F.sum("age").alias("sum"))
+
 
 def test_columns_present():
-    annotation = DataFrame['name': str, 'age': int]
-    assert annotation.columns == {'name', 'age'}
+    annotation = DataFrameNew["name":str, "age":int]
+    assert annotation.columns == {"name", "age"}
+
 
 def test_create_objects():
     user = User(**users_data[0])
 
-    assert user.name == users_data[0]['name']
-    assert isinstance(user, DataFrame)
+    assert user.name == users_data[0]["name"]
+    assert isinstance(user, DataFrameNew)
 
-    assert users[0].name == users_data[0]['name']
-    assert users[0].age == users_data[0]['age']
-    assert users[1].age == users_data[1]['age']
+    assert users[0].name == users_data[0]["name"]
+    assert users[0].age == users_data[0]["age"]
+    assert users[1].age == users_data[1]["age"]
+
 
 def test_function_defition():
     df = spark.createDataFrame(users)
--- typed_pyspark/__init__.py
+++ typed_pyspark/__init__.py
@@ -1,8 +1,11 @@
-from typing import List, Generic, get_type_hints
-import typing
-from pyspark.sql import DataFrame as DataFrameOrig
+from __future__ import annotations
+
 import inspect
-import attr
+import typing
+from typing import Any, Generic, List, NewType, TypeVar, get_type_hints
+
+from pyspark.sql import DataFrame as DataFrameOrig
+
 
 def _get_column_types(p):
     columns = set()
@@ -13,27 +16,34 @@ def _get_column_types(p):
     elif isinstance(p, slice):
         columns.add(p.start)
         if not inspect.isclass(p.stop):
-            raise TypeError("Column type hints must be classes, error with %s" % repr(p.stop))
+            raise TypeError(
+                "Column type hints must be classes, error with %s" % repr(p.stop)
+            )
         dtypes[p.start] = p.stop
     elif isinstance(p, (list, set)):
         for el in p:
             subcolumns, subdtypes = _get_column_types(el)
             columns |= subcolumns
             dtypes.update(subdtypes)
-    elif isinstance(p, DatasetMeta):
+    elif isinstance(p, DataFrameMeta):
         columns |= p.columns
         dtypes.update(p.dtypes)
     else:
-        raise TypeError("Dataset[col1, col2, ...]: each col must be a string, list or set.")
+        raise TypeError(
+            "Dataset[col1, col2, ...]: each col must be a string, list or set."
+        )
 
     return columns, dtypes
 
-class DatasetMeta(type):
+
+class DataFrameMeta(type):
     def __new__(metacls, name, bases, namespace, **kargs):
         return super().__new__(metacls, name, bases, namespace)
 
-    def __getitem__(self, parameters):
-        if hasattr(self, '__origin__') and (self.__origin__ is not None or self._gorg is not Dataset):
+    def __getitem__(self, parameters):  # type: ignore
+        if hasattr(self, "__origin__") and (
+            self.__origin__ is not None or self._gorg is not Dataset
+        ):
             return super().__getitem__(parameters)
         if parameters == ():
             return super().__getitem__(())
@@ -48,7 +58,7 @@ class DatasetMeta(type):
 
         columns, dtypes = _get_column_types(parameters)
 
-        meta = DatasetMeta(self.__name__, self.__bases__, {})
+        meta = DataFrameMeta(self.__name__, self.__bases__, {})
         meta.only_specified = only_specified
         meta.columns = columns
         meta.dtypes = dtypes
@@ -56,20 +66,21 @@ class DatasetMeta(type):
         return meta
 
 
-
 def class_annotation(cls):
     print("class annotation happening")
     return cls
 
-class DataFrame(DataFrameOrig, extra=Generic, metaclass=DatasetMeta):
 
+T = TypeVar("T", contravariant=True)
+
+
+class DataFrameNew(DataFrameOrig, extra=Generic[T], metaclass=DataFrameMeta):
     def __init__(self, *args, **kwargs):
         for name, value in kwargs.items():
-            print(name, value)
             setattr(self, name, value)
 
     @classmethod
-    def from_data(cls, data: List[dict]) -> List['DataFrame']:
+    def from_data(cls, data: List[dict]) -> List["DataFrameNew"]:
         result = []
         for row in data:
             rowobj = cls(**row)
@@ -78,7 +89,7 @@ class DataFrame(DataFrameOrig, extra=Generic, metaclass=DatasetMeta):
         return result
 
     def __getattr__(self, name):
-           return self[name]
+        return self[name]
 
     def __getitem__(self, items):
         return None
