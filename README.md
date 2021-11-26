4a87cd0480bf05bbd48cd212c4ec8e80b8036154
JeanCarloMachado <jean.machado@getyourguide.com>
2021-11-26 09:54:16 +0100

    add plugin

---
+++ README.md
@@ -0,0 +1,7 @@
+# TypedPyspark
+
+
+Contains a set of abstractions to type dataframes in pyspark.
+
+
+Inspired by [dataenforce](https://github.com/CedricFR/dataenforce) which provides similar functionality for pandas.
---
+++ typed_pyspark/mypy_plugin.py
@@ -0,0 +1,25 @@
+
+from __future__ import annotations
+
+from typing import Callable
+
+from mypy.plugin import Plugin
+
+
+class CustomPlugin(Plugin):
+
+    def main(self, func):
+        return func.api.fail("test jean")
+        #return func.default_return_type
+
+
+    def get_function_hook(self, name):
+        print("JEAN: " + name)
+        if name == 'DF':
+            return self.main
+        else:
+            return None
+
+
+def plugin(version):
+    return CustomPlugin
